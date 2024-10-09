3e5b4b78045990d5c2bb059bd6cb54cbfbd66100
TomPits23 <pepito.250107@dasmarinas.sti.edu.ph>
2024-10-09 21:49:29 +0800

    nag add ng user file
    wews

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,5 @@
 <?php 
 
-    // require('inc/essentials.php'); 
-    // require('inc/db_config.php'); 
-    
-    
-    // session_start();
-    //     if((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){ redirect('dashboard.php');   
-    //         echo 
-    //         redirect('dashboard.php');
-    //     }
-
-
 ?>
 
 
@@ -53,27 +42,6 @@
 
         <?php 
 
-            // if(isset($_POST['login'])) {
-            //     $frm_data = filteration($_POST);
-
-                
-            //     $query = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
-            //     $values = [$frm_data['admin_name'], $frm_data['admin_pass']]; 
-
-            //     $res = select($query, $values, "ss");
-
-            //     if($res->num_rows == 1) {
-            //         $row = mysqli_fetch_assoc($res); 
-
-                
-            //         $_SESSION['adminLogin'] = true;
-            //         $_SESSION['adminId'] = $row['sr_no'];
-            //         redirect('dashboard.php');
-                
-            //     } else {
-            //         alert('error', 'Login failed - Invalid Credentials!');
-            //     }
-            // }
 
             
         ?>
