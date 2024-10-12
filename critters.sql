85c1e89da596456a162e4d46d2a5f6f833f7d0a5
amiel0606 <lapid.167399@dasmarinas.sti.edu.ph>
2024-10-12 23:05:25 +0800

    Update sql
    SQl DROP

--- critters.sql
+++ critters.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2024 at 02:54 AM
+-- Generation Time: Oct 12, 2024 at 05:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,8 +31,41 @@ CREATE TABLE `tbl_bookings` (
   `id` int(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `slot` int(255) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `description` varchar(255) NOT NULL,
+  `categories` varchar(255) NOT NULL,
+  `service` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_bookings`
+--
+
+INSERT INTO `tbl_bookings` (`id`, `name`, `slot`, `description`, `categories`, `service`, `img`) VALUES
+(1, 'hehe', 2, 'wdzaxcqw', 'ewahhahw,sa sample itu', 'Sample po haha', '670a7688448574.84000547-1728738952.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_categories`
+--
+
+CREATE TABLE `tbl_categories` (
+  `category_id` int(255) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `category_parent` varchar(255) NOT NULL,
+  `category_price` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_categories`
+--
+
+INSERT INTO `tbl_categories` (`category_id`, `category_name`, `category_parent`, `category_price`) VALUES
+(21, 'ewahhahw', '10', 312),
+(26, 'sa sample itu', '10', 32),
+(27, 'sa 2nd itu', '13', 1123),
+(28, 'sa 2nd pa ulit', '13', 312);
 
 -- --------------------------------------------------------
 
@@ -59,21 +92,22 @@ INSERT INTO `tbl_images` (`id`, `img1`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_offers`
+-- Table structure for table `tbl_pets`
 --
 
-CREATE TABLE `tbl_offers` (
-  `offer_id` int(255) NOT NULL,
-  `offer_name` varchar(255) NOT NULL,
-  `offer_image` varchar(255) NOT NULL
+CREATE TABLE `tbl_pets` (
+  `id` int(255) NOT NULL,
+  `petType` varchar(255) NOT NULL,
+  `petName` varchar(255) NOT NULL,
+  `owner_ID` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tbl_offers`
+-- Dumping data for table `tbl_pets`
 --
 
-INSERT INTO `tbl_offers` (`offer_id`, `offer_name`, `offer_image`) VALUES
-(20, 'Blood test', '66fe0e6d764957.98187899.png');
+INSERT INTO `tbl_pets` (`id`, `petType`, `petName`, `owner_ID`) VALUES
+(1, 'Dog', 'Stark', 1);
 
 -- --------------------------------------------------------
 
@@ -85,6 +119,7 @@ CREATE TABLE `tbl_services` (
   `service_id` int(255) NOT NULL,
   `service_name` varchar(255) NOT NULL,
   `service_description` varchar(255) NOT NULL,
+  `service_price` int(255) NOT NULL,
   `service_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -92,9 +127,55 @@ CREATE TABLE `tbl_services` (
 -- Dumping data for table `tbl_services`
 --
 
-INSERT INTO `tbl_services` (`service_id`, `service_name`, `service_description`, `service_image`) VALUES
-(7, 'Vaccination', 'Pharvo', '66fe0b445268b0.33572491.png'),
-(8, 'Checkup', 'Tootbrush', '66fe0e7d797b70.54119985.png');
+INSERT INTO `tbl_services` (`service_id`, `service_name`, `service_description`, `service_price`, `service_image`) VALUES
+(10, 'Sample po haha', 'Sample po haha', 189, '67098d646eb1b2.81254004.png'),
+(13, '2nd service', '2nd service', 69, '670a588ecd9839.57503471.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_setappointment`
+--
+
+CREATE TABLE `tbl_setappointment` (
+  `appointment_id` int(255) NOT NULL,
+  `booking_date` varchar(255) NOT NULL,
+  `ownerName` varchar(255) NOT NULL,
+  `booking_id` varchar(255) NOT NULL,
+  `petType` varchar(255) NOT NULL,
+  `petName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_setappointment`
+--
+
+INSERT INTO `tbl_setappointment` (`appointment_id`, `booking_date`, `ownerName`, `booking_id`, `petType`, `petName`) VALUES
+(4, '2024-10-26', 'Amiel Carhyl Lapid', '1', 'Dog', 'Stark'),
+(5, '2024-10-16', 'Amiel Carhyl Lapid', '1', 'Dog', 'Stark');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `petType` varchar(255) NOT NULL,
+  `petName` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `username`, `password`, `petType`, `petName`, `firstName`, `lastName`) VALUES
+(1, 'amiel@gmail.com', '$2y$10$IGSOgLX9.X5un5l.UlbVceQ0mTgFSAcgURCZkxatjsftSShUnR9wu', 'Dog', 'Stark', 'Amiel Carhyl', 'Lapid');
 
 --
 -- Indexes for dumped tables
@@ -107,22 +188,40 @@ ALTER TABLE `tbl_bookings`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_categories`
+--
+ALTER TABLE `tbl_categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
 -- Indexes for table `tbl_images`
 --
 ALTER TABLE `tbl_images`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbl_offers`
+-- Indexes for table `tbl_pets`
 --
-ALTER TABLE `tbl_offers`
-  ADD PRIMARY KEY (`offer_id`);
+ALTER TABLE `tbl_pets`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_services`
 --
 ALTER TABLE `tbl_services`
   ADD PRIMARY KEY (`service_id`);
+
+--
+-- Indexes for table `tbl_setappointment`
+--
+ALTER TABLE `tbl_setappointment`
+  ADD PRIMARY KEY (`appointment_id`);
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -132,7 +231,13 @@ ALTER TABLE `tbl_services`
 -- AUTO_INCREMENT for table `tbl_bookings`
 --
 ALTER TABLE `tbl_bookings`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_categories`
+--
+ALTER TABLE `tbl_categories`
+  MODIFY `category_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `tbl_images`
@@ -141,16 +246,28 @@ ALTER TABLE `tbl_images`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `tbl_offers`
+-- AUTO_INCREMENT for table `tbl_pets`
 --
-ALTER TABLE `tbl_offers`
-  MODIFY `offer_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+ALTER TABLE `tbl_pets`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_services`
 --
 ALTER TABLE `tbl_services`
-  MODIFY `service_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `service_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tbl_setappointment`
+--
+ALTER TABLE `tbl_setappointment`
+  MODIFY `appointment_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
